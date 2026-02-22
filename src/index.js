abfc8b9f0743334a1f1f6242e2d6394c16152643
Falcosn28 <205811111+Falcosn28@users.noreply.github.com>
2026-02-22 22:00:37 +0100

    basic files added

--- src/index.js
+++ src/index.js
@@ -1,29 +1,2 @@
 
 import "./styles.css";
-import {loadHome} from "./home.js"
-import {loadMenu} from "./menu.js"
-import {loadAbout} from "./about-us.js"
-
-const pageHome = document.querySelector(".page-home")
-const pageMenu = document.querySelector(".page-menu")
-const pageAbout = document.querySelector(".page-about")
-const Content = document.querySelector("#content");
-
-
-pageHome.addEventListener("click", () => {
-  Content.innerHTML = ""
-  loadHome()
-})
-
-pageMenu.addEventListener("click", () => {
-  Content.innerHTML = ""
-  loadMenu()
-})
-
-pageAbout.addEventListener("click", () => {
-  Content.innerHTML = ""
-  loadAbout()
-})
-
-loadHome()
-
